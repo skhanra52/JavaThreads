39cdaa434e7b42982db0756763d7be367f62f0db
Suman Kalyan Khanra <skhanra52@gmail.com>
2026-03-02 07:12:52 +0530

    Multi threading skeleton

---
+++ src/main/java/com/skhanra52/Main.java
@@ -0,0 +1,10 @@
+package com.skhanra52;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Thread currentThread = Thread.currentThread();
+        System.out.println(currentThread.getClass().getName());
+
+    }
+}
